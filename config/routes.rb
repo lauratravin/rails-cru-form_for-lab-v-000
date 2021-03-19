bf3ca8147d373ad16be20eed340d5313f364ee28
Laura Messa Travin <laura.travin@gmail.com>
2021-03-19 02:06:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'artist/index'
+
+  get 'artist/show'
+
+  get 'artist/new'
+
+  get 'artist/create'
+
+  get 'artist/edit'
+
+  get 'artist/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
